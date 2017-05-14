b6e96af27645049cb109ce93302aaed536e4723f
JerryWang304 <wang_feng_bro@163.com>
2017-05-14 14:32:25 +0800

    add problem 1

---
+++ 1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#encoding: utf-8
+
+# 在图右上角添加数字
+from PIL import Image, ImageDraw, ImageFont
+def add_number(picture):
+    image = Image.open(picture)
+    print image.format
+    #image.show()
+    width, height = image.size
+    print "width = %d, height = %d" % (width, height)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("Roboto-Light.ttf",90)
+    draw.text((width-100,0),"10",font=font, fill='#FF0000' )
+    #image.save('result.jpg')
+    image.show()
+if __name__ == "__main__":
+    add_number("avatar.jpeg")
